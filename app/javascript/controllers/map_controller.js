e927b5578a10eb3df5355fa9e5db85819145cbd0
Riccardo Giomi <me.personally.myself@gmail.com>
2024-03-29 10:34:20 +0100

    Show basic map in new/edit views, no interaction with the form yet.
    Using Leaflet + OpenStreetMap, as Leaflet seems easier to integrate, and
    as such a better fit for a first test application.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
+import L from "leaflet"
 
 export default class extends Controller {
   static targets = []
 
-  connect() {}
+  connect() {
+    this.initializeMap()
+  }
+
+  initializeMap() {
+    this.map = L.map('map', { attributionControl: false }).setView([51.505, -0.09], 13)
+
+    L.control.attribution({ prefix: false })
+      .setPrefix('Realized using <a href="https://leafletjs.com" title="A JavaScript library for interactive maps">Leaflet</a>')
+      .addTo(this.map)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: ' Map from &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map)
+  }
 }
